9f23f49a85c30914071cfb76f73915b8b0b1551b
Yuz <wwowkdrr@gmail.com>
2025-07-14 14:44:26 +0800

    增加管理者登入後的頁面顯示

--- front/login.php
+++ front/login.php
@@ -1,33 +1,38 @@
 <fieldset>
     <legend>會員登入</legend>
-    <form action="">
-        <table>
-            <tr>
-                <td>帳號</td>
-                <td><input type="text" name="acc" id="acc"></td>
-            </tr>
-            <tr>
-                <td>密碼</td>
-                <td><input type="text" name="pw" id="pw"></td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="button" value="登入" onclick='login()'>
-                    <input type="reset" value="清除">
-                </td>
-                <td>
-                    <a href="?do=forgot">忘記密碼</a>
-                    <a href="?do=reg">註冊會員</a>
-                </td>
-            </tr>
-        </table>
-    </form>
+
+<form>
+    <table>
+        <tr>
+            <td>帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td>密碼</td>
+            <td>
+                <input type="password" name="pw" id="pw">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="button" value="登入" onclick='login()'>
+                <input type="reset" value="清除">
+            </td>
+            <td>
+                <a href="?do=forgot">忘記密碼</a>
+                    |
+                <a href="?do=reg">註冊會員</a>
+            </td>
+        </tr>
+    </table>
+</form>
+
 </fieldset>
 <script>
-    function login() {
-        let data = {
-            acc: $("#acc").val(),
-            pw: $("#pw").val()
+    function login(){
+        let data={
+            acc:$("#acc").val(),
+            pw:$("#pw").val()
         }
 
         $.get("./api/chk_acc.php",data,(res)=>{
@@ -36,7 +41,7 @@
                     if(parseInt(res)){
                         //登入成功
                         if(data.acc=='admin'){
-                            location.href="back.php";
+                            location.href="back.php"
                         }else{
                             location.href="index.php";
                         }
@@ -45,9 +50,13 @@
                         location.href="index.php?do=login";
                     }
                 })
+
+
             }else{
-                alert("查無帳號");
+                alert("查無帳號")
             }
         })
+
+
     }
 </script>
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
-﻿<?php include_once "api/db.php";
-?>
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<?php include_once "api/db.php"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -10,24 +8,23 @@
 
 	<title>健康促進網</title>
 	<link href="./css/css.css" rel="stylesheet" type="text/css">
-	<script src="./js//jquery-3.4.1.js"></script>
+	<script src="./js/jquery-1.9.1.min.js"></script>
 	<script src="./js/js.js"></script>
 </head>
 
 <body>
-	<div id="alerr"
-		style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
+	<div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
 		<pre id="ssaa"></pre>
 	</div>
-	<!-- <iframe name="back" style="display:none;"></iframe> -->
+
 	<div id="all">
 		<div id="title">
-			<?=date("m 月 d 日 l");?> | 今日瀏覽: <?=$Visit->find(['date'=>date("Y-m-d")])['visit'];?> | 累積瀏覽: <?=$Visit->sum("visit");?>
+			<?= date("m 月 d 日 l"); ?> | 今日瀏覽: <?= $Visit->find(['date' => date("Y-m-d")])['visit']; ?> | 累積瀏覽: <?= $Visit->sum("visit"); ?>
 			<a href="index.php" style='float:right'>回首頁</a>
 		</div>
 		<div id="title2">
 			<a href="index.php">
-				<img src="./icon/02B01.jpg" title="健康促進網-回首頁" alt="健康促進網-回首頁">
+				<img src="./icon/02B01.jpg" title='健康促進網-回首頁' alt="健康促進網-回首頁">
 			</a>
 		</div>
 		<div id="mm">
@@ -40,33 +37,38 @@
 			</div>
 			<div class="hal" id="main">
 				<div>
-					
-					<marquee style='width:78%; display:inline-block;'>
-						請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章
-					</marquee>					
+					<marquee style='width:78%; display:inline-block'>請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章</marquee>
 					<span style="width:20%; display:inline-block;">
-					
-					<?php
-					if(isset($_SESSION['login'])){
-						echo "歡迎，".$_SESSION['login'];
-						echo "<button>登出</button>";
-					}else{
-						echo "<a href='?do=login'>會員登入</a>";
-					}
-						
-					?>
-						<!-- <a href="?do=login">會員登入</a> -->
+
+						<?php
+						if (isset($_SESSION['login'])) {
+							if ($_SESSION['login'] == 'admin') {
+								echo "歡迎，" . $_SESSION['login'];
+								echo "<br>";
+								echo "<button>管理</button>";
+								echo "|";
+								echo "<button>登出</button>";
+							} else {
+								echo "歡迎，" . $_SESSION['login'];
+								echo "<button>登出</button>";
+							}
+						} else {
+
+							echo "<a href='?do=login'>會員登入</a>";
+						}
+
+						?>
 					</span>
 					<div class="">
-							<?php
-							$do=$_GET['do'] ?? 'main';
-							$file="./front/".$do.".php";
-							if(file_exists($file)){
-								include $file;
-							}else{
-								include "./front/main.php";
-							}
-							?>
+						<?php
+						$do = $_GET['do'] ?? 'main';
+						$file = "./front/" . $do . ".php";
+						if (file_exists($file)) {
+							include $file;
+						} else {
+							include "./front/main.php";
+						}
+						?>
 					</div>
 				</div>
 			</div>
