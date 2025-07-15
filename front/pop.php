2deb5906c43b455731dd6b464d8e60c647fea638
Yuz <wwowkdrr@gmail.com>
2025-07-15 15:33:37 +0800

    完成人氣文章彈出文章功能

--- front/pop.php
+++ front/pop.php
@@ -1,7 +1,28 @@
 <div class="nav" style="margin-bottom:20px;">
     目前位置：首頁 > 人氣文章區 >
 </div>
+<style>
+    .title {
+        cursor: pointer;
+        color: blue;
+    }
 
+    .title:hover {
+        text-decoration: underline;
+        color: green;
+    }
+    .pop {
+			background: rgba(51, 51, 51, 0.8);
+			color: #FFF;
+			/* min-height: 100px; */
+            height: 400px;
+			width: 500px;
+			position: fixed;
+			display: none;
+			z-index: 9999;
+			overflow: auto;
+		}
+</style>
 <table style="width: 95%; margin:auto;">
     <tr class="ct">
         <td width="20%">標題</td>
@@ -9,47 +30,63 @@
         <td>人氣</td>
     </tr>
     <?php
-    
-     $total = $News->count();
-        $div = 3;
-        $pages = ceil($total / $div);
-        $now = $_GET['p'] ?? 1;
-        $start = ($now - 1) * $div;
-        $rows = $News->all(" order by `good` desc limit $start,$div");
-        foreach ($rows as $idx => $row):
-    
-    ?>
-    <tr>
-        <td><?=$row['title'];?></td>
-        <td>
-            <div class="short"><?=mb_substr($row['text'],0,30);?>...</div>
-            <div class="all"></div>
 
-        </td>
-        <td></td>
-    </tr>
+    $total = $News->count();
+    $div = 5;
+    $pages = ceil($total / $div);
+    $now = $_GET['p'] ?? 1;
+    $start = ($now - 1) * $div;
+    $rows = $News->all(" order by `good` desc limit $start,$div");
+    foreach ($rows as $idx => $row):
+
+    ?>
+        <tr>
+            <td class="title"><?= $row['title']; ?></td>
+            <td>
+                <div class="short"><?= mb_substr($row['text'], 0, 30); ?>...</div>
+                <div class="all">
+                    <div id="alerr" class="pop">
+                        <h2><?=$Type[$row['type']];?></h2>
+                        <pre id="ssaa">
+                            <?=$row['text'];?></pre>
+                    </div>
+                </div>
+
+            </td>
+            <td></td>
+        </tr>
 
     <?php
     endforeach;
     ?>
 </table>
 <div>
-        <?php
+    <?php
 
-        if ($now - 1 > 0) {
-            echo "<a href='?do=pop&p=" . ($now - 1) . "' style='font-size:18px'> < </a>";
+    if ($now - 1 > 0) {
+        echo "<a href='?do=pop&p=" . ($now - 1) . "' style='font-size:18px'> < </a>";
+    }
+
+    for ($i = 1; $i <= $pages; $i++) {
+        $size = ($i == $now) ? '24px' : '18px';
+        echo "<a href='?do=pop&p=$i' style='font-size:{$size}'> $i </a>";
+    }
+
+
+    if ($now + 1 <= $pages) {
+        echo "<a href='?do=pop&p=" . ($now + 1) . "' style='font-size:18px'> > </a>";
+    }
+    ?>
+
+
+</div>
+<script>
+    $(".title").hover(
+        function(){
+            $(this).next().find(".pop").show()
+        },
+        function(){
+            $(this).next().find(".pop").hide()
         }
-
-        for ($i = 1; $i <= $pages; $i++) {
-            $size = ($i == $now) ? '24px' : '18px';
-            echo "<a href='?do=pop&p=$i' style='font-size:{$size}'> $i </a>";
-        }
-
-
-        if ($now + 1 <= $pages) {
-            echo "<a href='?do=pop&p=" . ($now + 1) . "' style='font-size:18px'> > </a>";
-        }
-        ?>
-
-
-    </div>
+    )
+</script>
--- index.php
+++ index.php
@@ -10,12 +10,22 @@
 	<link href="./css/css.css" rel="stylesheet" type="text/css">
 	<script src="./js/jquery-1.9.1.min.js"></script>
 	<script src="./js/js.js"></script>
+	<!-- <style>
+		.pop {
+			background: rgba(51, 51, 51, 0.8);
+			color: #FFF;
+			min-height: 100px;
+			width: 300px;
+			position: fixed;
+			display: none;
+			z-index: 9999;
+			overflow: auto;
+		}
+	</style> -->
 </head>
 
 <body>
-	<div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
-		<pre id="ssaa"></pre>
-	</div>
+	
 
 	<div id="all">
 		<div id="title">
