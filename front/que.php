1fe5556b174a145cec17b6f74eb5335a1919b794
Yuz <wwowkdrr@gmail.com>
2025-07-21 09:44:52 +0800

    在投票頁面顯示題目

--- front/que.php
+++ front/que.php
@@ -16,20 +16,19 @@
             <td><?=$key+1;?></td>
             <td><?=$row['text'];?></td>
             <td><?=$row['vote'];?></td>
-            <td><a href='do=result&id=<?=$row['id'];?>'>結果</td>
+            <td><a href='?do=result&id=<?=$row['id'];?>'>結果</td>
             <td>
-                <?php
-                if(isset($_SESSION['login'])):
-                ?>
-                <a href="do=vote&id=<?$row['id'];?>">參與投票</a>
-                <?php
-                else:    
-                ?>
-                <a href="?do=login">請先登入</a>
-                <?php endif; ?>
+            <?php 
+            if(isset($_SESSION['login'])):
+            ?>
+            <a href="?do=vote&id=<?=$row['id'];?>">參與投票</a>
+            <?php else: ?>
+            <a href="?do=login">請先登入</a>
+            <?php  endif;   ?>
             </td>
         </tr>
-        <?php
+
+        <?php 
         endforeach;
         ?>
     </table>
